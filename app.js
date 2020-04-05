faf62a6588da78301e8926239f01c1da348ef2a0
Craig <cbcheesman@gmail.com>
2020-04-05 10:48:29 +1030

    Fixed the Player Table Names having index out by 1 at start

--- app.js
+++ app.js
@@ -89,8 +89,8 @@ io.on('connection', function (socket) {
       //create new player
       console.log(`player ${uuid} created`);
       players.push({ uuid, hand: [], socket: socket.id, name: `Player ${players.length + 1}` });
-      playerdata.push({ cardsInHand: 0, score: 0, wins: 0, name: `Player ${players.length + 1}`, uno: false });
       let newplayerindex = uuidToIndex(uuid);
+      playerdata.push({ cardsInHand: 0, score: 0, wins: 0, name: players[newplayerindex].name, uno: false });
       if (inProgress) {
         message(`new player ${uuidToName(uuid)} - joined halfway through a game`);
         players[newplayerindex].hand.push(pile.pop());
